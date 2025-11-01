1c09efd74fbe0391732453e81b1feadc1551664e
ryansousa-ls <ryansousa@aluno.uespi.br>
2025-11-01 13:58:14 -0300

    Atualização app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash, request
 from models import db, Produto
 from forms import ProdutoForm
-#RYAN HUGOXTOSO
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'sua_chave_secreta'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///loja.db'
@@ -11,7 +11,6 @@ db.init_app(app)
 
 with app.app_context():
     db.create_all()
-
 
 @app.route('/add_produto', methods=['GET', 'POST'])
 def add_produto():
@@ -28,6 +27,19 @@ def add_produto():
 def listar_produtos():
     produtos = Produto.query.all()
     return render_template('lista_produtos.html', produtos=produtos)
+
+# NOVA ROTA: editar produto
+@app.route('/editar_produto/<int:id>', methods=['GET', 'POST'])
+def editar_produto(id):
+    produto = Produto.query.get_or_404(id)
+    form = ProdutoForm(obj=produto)  # pré-preenche o formulário
+    if form.validate_on_submit():
+        produto.nome = form.nome.data
+        produto.preco = float(form.preco.data)
+        db.session.commit()
+        flash(f'Produto "{produto.nome}" atualizado com sucesso!', 'success')
+        return redirect(url_for('listar_produtos'))
+    return render_template('form_produto.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
